0a5936372f114fe364e78957df8ab875844b59a9
Adeyemo Johnson <125729236+crownjohnson@users.noreply.github.com>
2024-05-12 20:17:46 -0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -15,23 +15,23 @@ global.doc5 = 'application/pdf'
 global.doc6 = 'application/vnd.android.package-archive'
 
 //owmner v card
-global.owner = ['916909137213'] //ur owner number
-global.ownername = "🦄Dream Guy Xeon" //ur owner name
-global.ytname = "YT: Xeon" //ur yt chanel name
-global.socialm = "GitHub: DGXeon" //ur github or insta name
-global.location = "India, Mizoram, Aizawl" //ur location
+global.owner = ['2347038101531'] //ur owner number
+global.ownername = "Ceejay bot" //ur owner name
+global.ytname = "YT: ceejay tech" //ur yt chanel name
+global.socialm = "GitHub: crownjohnson" //ur github or insta name
+global.location = "Nigeria, ogun state" //ur location
 
 //bot bomdy 
-global.ownernomer = "916909137213" //ur number
+global.ownernomer = "2347038101531" //ur number
 global.premium = ['916909137213'] //ur premium number
 global.botname = 'Cheems Bot MD V6' //ur bot name
-global.linkz = "https://chat.whatsapp.com/HYj9wu5Jrv6CROxyeQbHoS" //your theme url which will be displayed on whatsapp
-global.websitex = "https://youtu.be/WiIqCdiDjFo" //ur website to be displayed
-global.botscript = 'https://github.com/DGXeon/CheemsBot-MD6' //script link
+global.linkz = "https://chat.whatsapp.com/HNKnosvMaUg5HBjiGZNVbY" //your theme url which will be displayed on whatsapp
+global.websitex = "https://www.youtube.com/channel/UCA-WaheZgRjhBv3GqUtRJoQ" //ur website to be displayed
+global.botscript = 'https://www.youtube.com/channel/UCA-WaheZgRjhBv3GqUtRJoQ' //script link
 global.themeemoji = "🦄" //ur theme emoji
-global.packname = "Sticker By" //ur sticker watermark packname
-global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\n🦄Dream Guy Xeon\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
-global.wm = "Xeon Bot Inc." //ur watermark
+global.packname = "Sticker By ceejay T" //ur sticker watermark packname
+global.author = "Cheems Bot\n\n\nA whatsapp bot developed by\ceejay tech\n\n\n\n\n\n\n\n       --------______-------\n\n\n\n\n\n\n\nContact No. : +916909137213" //ur sticker watermark author
+global.wm = "ceejay Bot Inc." //ur watermark
 
 // Other
 global.sessionName = 'session'
